27f4ca2da76c2535a29fddd9cc562f55e4cc8c06
baba-tope <65943942+baba-tope@users.noreply.github.com>
2022-07-18 00:14:51 -0500

    Update backend.tf
    Updated the organization name

--- backend.tf
+++ backend.tf
@@ -2,7 +2,7 @@ terraform {
   required_version = ">= 1.1.0"
   backend "remote" {
     hostname = "app.terraform.io"
-    organization = "ace_iac"
+    organization = "ace_iac-netops"
     workspaces {
       name = "ace-iac-day-zero"
     }
